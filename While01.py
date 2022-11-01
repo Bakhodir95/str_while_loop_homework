fe614940ae447a8f2b9b717b945c89b3d6f6d29b
Bahodir <bakhodir.turdimurodov@bk.ru>
2022-11-02 00:59:45 +0500

    -001

--- While01.py
+++ While01.py
@@ -1,9 +1,25 @@
-def main(s):
-    """
+
+"""
     A variable of type str is given. Find how many numbers it contains and return.
     Args:
         s: str
     Returns:
         int: return answer
     """
-    return
+
+
+def main(s):
+
+    i = 0
+    count = ""
+
+    while i < len(s):
+        if s[i].isdigit():
+            count += s[i]
+        i += 1
+
+    count = int(count)
+    return count
+
+
+print(main("4t54t4fgr"))
